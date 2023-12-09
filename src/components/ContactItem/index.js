deab3346e3199b7a13c27687ac00f3239049548e
nikhilsangemA <sangemnikhil15@gmail.com>
2023-12-09 16:47:00 +0000

    contactsAdd

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, toggleTheItem} = props
+  const {id, name, mobileNo, isFavorite} = contactDetails
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  const changeItem = () => {
+    toggleTheItem(id)
+  }
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          onClick={changeItem}
+          className="favorite-icon-container"
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
